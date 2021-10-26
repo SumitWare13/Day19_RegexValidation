4eef3966a13590107a3b8b4e5f31d2c902511a7e
Grid112 <sumitware1999@gmail.com>
2021-10-26 22:20:23 +0530

    Added: Last Name Validation

--- src/com/bridgelabz/UserRegistrationFormatValidation.java
+++ src/com/bridgelabz/UserRegistrationFormatValidation.java
@@ -16,7 +16,7 @@ public class UserRegistrationFormatValidation {
         String firstName = Sc.next();
 
         /* RegexFirstName Pattern have:
-            one capital letter
+            1st letter is mandatory capital letter
             Minimum 3 characters
         */
         String regexFirstName = "^[A-Z]{1}[a-z]{2,}";
@@ -25,7 +25,25 @@ public class UserRegistrationFormatValidation {
         Boolean firstNameIs = matcher.matches();
         System.out.println(firstNameIs);
         if (!firstNameIs){
-            System.out.print("Enter Name According To Pattern: [A-Z]{1}+[a-z]{2,}");
+            System.out.println("Enter First Name According To Pattern: [A-Z]{1}+[a-z]{2,}");
+        }
+    }
+    //Created A Method For Last Name Validation
+    public void lastNameValidation(){
+        System.out.print("Enter Last Name: " );
+        String lastName = Sc.next();
+
+        /* RegexLastName Pattern have:
+            1st letter is mandatory capital letter
+            Minimum 3 characters
+        */
+        String regexLastName = "^[A-Z]{1}[a-z]{2,}";
+        pattern = Pattern.compile(regexLastName);
+        matcher = pattern.matcher(lastName);
+        Boolean lastNameIs = matcher.matches();
+        System.out.println(lastNameIs);
+        if (!lastNameIs){
+            System.out.println("Enter Last Name According To Pattern: [A-Z]{1}+[a-z]{2,}");
         }
     }
 }
--- src/com/bridgelabz/UserRegistrationFormatValidationMain.java
+++ src/com/bridgelabz/UserRegistrationFormatValidationMain.java
@@ -5,13 +5,27 @@ import java.util.Scanner;
 public class UserRegistrationFormatValidationMain {
     public static void main(String[] args) {
         System.out.println("Welcome To User Registration Format Validation Using Regex Program");
-        UserRegistrationFormatValidation Validate = new UserRegistrationFormatValidation();
         Scanner Sc = new Scanner(System.in);
-        System.out.println("Enter 1 : to validate First Name ");
-        switch (Sc.nextInt()){
-            case 1:
-                Validate.firstNameValidation();
-                break;
+
+        UserRegistrationFormatValidation Validate = new UserRegistrationFormatValidation();
+
+        System.out.println("Enter 1 : to Validate First Name ");
+        System.out.println("Enter 2 : to Validate Last Name ");
+
+        int choice = 0;
+        while (choice == 0) {
+            switch (Sc.nextInt()) {
+                case 1:
+                    Validate.firstNameValidation();
+                    break;
+                case 2:
+                    Validate.lastNameValidation();
+                    break;
+                default:
+                    System.out.println("Enter Valid Choice");
+                    choice = 1;
+                    break;
+            }
         }
     }
 }
